5e1d83cf6aa859b3e149a5d7b01767ceb7c56fbb
ligm1208 <1015502016@qq.com>
2023-04-26 11:39:57 +0800

    完成后台主页开发

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+$(function() {
+    $.ajax({
+        methods: 'get',
+        url: '/my/userinfo',
+        success: function(res) {
+            if (res.status !== 0) return layui.layer.msg(res.message)
+            renderAvatar(res.data)
+
+        },
+
+
+    })
+
+    $("#btnLogout").on('click', function() {
+        layui.layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            localStorage.removeItem('token')
+            location.href = 'login.html'
+            layer.close(index);
+        });
+    })
+})
+
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html("欢迎&nbsp;&nbsp;" + name)
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        var first = name.charAt(0).toUpperCase()
+        $('.text-avatar').html(first).show()
+        $('.layui-nav-img').hide()
+
+    }
+}
